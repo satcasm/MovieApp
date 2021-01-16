1ccdcab2f666344ec57c54375db9d175a401aca5
satcasm <satyamsingh200151@gmail.com>
2021-01-16 07:03:43 +0000

    improved the stylings

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-
+const path = require('path');
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",(req,res)=>{
 	res.render("search");
@@ -13,11 +14,9 @@ app.get("/results",(req,res)=>{
 	const url = `http://www.omdbapi.com/?apikey=thewdb&s=${query}`;
   axios.get(url)
   .then(function (body) {
-	  var resa = body.data;
-	  //console.log(typeof data);
-	  //console.log(resa);
-	  if (resa.Response === "True") {
-               res.render("results",{result : resa});
+	  const result = body.data;
+	  if (result.Response === "True") {
+               res.render("results",{result});
             // if not, pass null to the view, which will be handled by ejs to say no movies found
             } else {
                 res.render("results", {result: null});
@@ -29,10 +28,10 @@ app.get("/results",(req,res)=>{
     console.log(error);
 	  res.send("Wrong movie name!");
   })
-  .finally(function () {
-    // always executed
-  });
 });
+app.get("*",(req,res)=>{
+	res.render("error");
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
